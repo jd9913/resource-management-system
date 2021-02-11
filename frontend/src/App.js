7ed335721554d751586b7b85207c3b9ee2f22c52
Jennifer Schlierman <jd9913@gmail.com>
2021-02-11 15:27:36 -0700

    initial header-footer setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
-  return (
-    <>
-    <h1>Welcome!</h1>
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<h1>Welcome!</h1>
+			<Footer />
+		</>
+	);
 }
 
 export default App;
